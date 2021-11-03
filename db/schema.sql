560f6a1a77cf1211fac91ed310b1452650d87128
Hazelton Chrome <rdupont1623@gmail.com>
2021-11-03 12:21:57 -0400

    Initial creation of app - framework started for DB, nodes installed, Class' made

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+
+DROP DATABASE IF EXISTS employeetrack;
+
+CREATE DATABASE employeetrack;
+
+USE employeetrack;
+
+CREATE TABLE department (
+
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    
+)
+
---
+++ server.js
@@ -0,0 +1,12 @@
+// importing inquirer
+const inquirer = require('inquirer');
+
+// importing database connection
+let database = require('./db/connection');
+
+// importing mysql2
+let mysql = require('mysql2');
+
+// importing tool to make chart in terminal
+ctable = require('console.table');
+
